920379834fd9b63bd559bd1c17d9711942c52626
VB666 <vict.gamboa@duocuc.cl>
2023-07-13 20:13:28 -0400

    Se logro Correjir el Fdo Alfin

--- GuitarCosmic/settings.py
+++ GuitarCosmic/settings.py
@@ -1,3 +1,4 @@
+import os
 """
 Django settings for GuitarCosmic project.
 
@@ -117,7 +118,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
